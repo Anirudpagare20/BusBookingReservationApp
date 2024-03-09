53becd49ab9b48b6de841e8750fa7928d76fc12b
anirud <127181241+Anirudpagare20@users.noreply.github.com>
2024-03-10 01:23:29 +0530

    Your commit message here

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
-const bookingController = require('./controllers/BookingController'); // Importing booking controller
-const userController = require('./controllers/userController'); // Importing user controller
+const bookingControllers = require('./controllers/BookingControllers'); // Importing booking controller
+const userControllers = require('./controllers/userControllers'); // Importing user controller
 const fetchUser = require('./middleware/fetchuser'); // Importing middleware for fetching user
 const BusData = require('../Data/BusData'); // Importing bus data
 
@@ -18,24 +18,24 @@ router.get('/bus-info', (req, res) => {
 
 //************ User requests *********************
 
-router.post('/user/signup', userController.userValidationRules, userController.addNewUser); // Endpoint for user signup
+router.post('/user/signup', userControllers.userValidationRules, userControllers.addNewUser); // Endpoint for user signup
 
-router.post('/user/login', userController.userLoginRules, userController.login); // Endpoint for user login
+router.post('/user/login', userControllers.userLoginRules, userControllers.login); // Endpoint for user login
 
-router.post('/get-user', fetchUser, userController.getUser); // Endpoint to get user details
+router.post('/get-user', fetchUser, userControllers.getUser); // Endpoint to get user details
 
 //************ Bookings requests *********************
 
-router.get('/bookings/get', bookingController.bookings); // Endpoint to get all bookings
+router.get('/bookings/get', bookingControllers.bookings); // Endpoint to get all bookings
 
-router.get('/bookings/date/:date', bookingController.bookingsByDate); // Endpoint to get bookings by date
+router.get('/bookings/date/:date', bookingControllers.bookingsByDate); // Endpoint to get bookings by date
 
-router.get('/bookings/search', bookingController.bookingParameter); // Endpoint to search bookings with parameters
+router.get('/bookings/search', bookingControllers.bookingParameter); // Endpoint to search bookings with parameters
 
 router.post('/bookings/new',
     // fetchUser, // Uncomment this line if you want to fetch user before adding booking
-    bookingController.bookingValidationRules, // Applying validation rules for adding a booking
-    bookingController.addBooking // Endpoint to add a new booking
+    bookingControllers.bookingValidationRules, // Applying validation rules for adding a booking
+    bookingControllers.addBooking // Endpoint to add a new booking
 );
 
 app.use('/api/v1', router); // Mounting the router at /api/v1 base path
